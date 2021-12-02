af0b68f6184acc6d18841222c30374ca634dbfb4
dpak1999 <deepakkumardash313@gmail.com>
2021-12-02 20:45:46 +0530

    fix styling for movie card

--- src/App.js
+++ src/App.js
@@ -5,28 +5,25 @@ import MovieCard from './MovieCard';
 
 function App() {
   return (
-    <>
-      <div className="bg-gradient-to-b from-red-300 to-purple-700 min-h-screen overflow-hidden">
-        <h1 className="text-white text-4xl text-center underline italic mt-4 font-bold">
-          Favorite movies
-        </h1>
-        <div className="text-center mt-14">
-          {Object.keys(movies).map((item) => (
-            <span
-              key={item}
-              className="mr-5 border rounded px-4 py-2 text-2xl cursor-pointer text-white hover:bg-blue-500"
-            >
-              {item}
-            </span>
-          ))}
-        </div>
-        <hr className="mt-8" />
+    <div className="bg-purple-500 min-h-screen overflow-hidden">
+      <h1 className="text-white text-4xl text-center underline italic mt-4 font-bold">
+        Favorite movies
+      </h1>
+      <div className="text-center mt-14">
+        {Object.keys(movies).map((item) => (
+          <span
+            key={item}
+            className="mr-5 border rounded px-4 py-2 text-2xl cursor-pointer text-white hover:bg-blue-500"
+          >
+            {item}
+          </span>
+        ))}
       </div>
-
+      <hr className="mt-8" />
       {Object.values(movies).map((movie, idx) => (
         <MovieCard key={idx} movie={movie} />
       ))}
-    </>
+    </div>
   );
 }
 
