a7758bfc70faaebca59031de4509d0f04e9d7b1d
dpak1999 <deepakkumardash313@gmail.com>
2021-12-02 20:29:54 +0530

    add : movie card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 /** @format */
 
+import movies from './data';
+import MovieCard from './MovieCard';
+
 function App() {
   return (
-    <div className="bg-gradient-to-b from-red-300 to-purple-700 min-h-screen flex">
-      <h1 className="text-white font-bold text-4xl m-auto italic font-serif">
-        This is a react tailwind starter template
-      </h1>
-    </div>
+    <>
+      <div className="bg-gradient-to-b from-red-300 to-purple-700 min-h-screen overflow-hidden">
+        <h1 className="text-white text-4xl text-center underline italic mt-4 font-bold">
+          Favorite movies
+        </h1>
+        <div className="text-center mt-14">
+          {Object.keys(movies).map((item) => (
+            <span
+              key={item}
+              className="mr-5 border rounded px-4 py-2 text-2xl cursor-pointer text-white hover:bg-blue-500"
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+        <hr className="mt-8" />
+      </div>
+
+      {Object.values(movies).map((movie, idx) => (
+        <MovieCard key={idx} movie={movie} />
+      ))}
+    </>
   );
 }
 
---
+++ src/MovieCard.js
@@ -0,0 +1,8 @@
+/** @format */
+
+const MovieCard = ({ movie }) => {
+  console.log(movie.name);
+  return <div></div>;
+};
+
+export default MovieCard;
