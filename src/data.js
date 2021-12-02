0a481d14dfd2cf9e72b0d65fd6e7dfa533e9f01c
dpak1999 <deepakkumardash313@gmail.com>
2021-12-02 20:03:02 +0530

    add : dummy data

---
+++ src/data.js
@@ -0,0 +1,89 @@
+/** @format */
+
+const movies = {
+  Action: [
+    {
+      name: 'Venom: Let There Be Carnage',
+      imdb: '6.1',
+      plot: 'Eddie Brock is still struggling to coexist with the shape-shifting extraterrestrial Venom. When deranged serial killer Cletus Kasady also becomes host to an alien symbiote, Brock and Venom must put aside their differences to stop his reign of terror.',
+    },
+    {
+      name: 'Red Notice',
+      imdb: '6.4',
+      plot: "In the world of international crime, an Interpol agent attempts to hunt down and capture the world's most wanted art thief",
+    },
+    {
+      name: 'Shang-Chi and The Legend of The Ten Rings ',
+      imdb: '7.6',
+      plot: "Martial-arts master Shang-Chi confronts the past he thought he left behind when he's drawn into the web of the mysterious Ten Rings organization.",
+    },
+    {
+      name: 'Tenet',
+      imdb: '7.4',
+      plot: 'When a few objects that can be manipulated and used as weapons in the future fall into the wrong hands, a CIA operative, known as the Protagonist, must save the world.',
+    },
+  ],
+  Mystery: [
+    {
+      name: 'Sherlock Holmes',
+      imdb: '7.6',
+      plot: 'Detective Sherlock Holmes and his partner, Dr Watson, send Blackwood, a serial killer, to the gallows. However, they are shocked to learn that he is back from the dead and must pursue him again.',
+    },
+    {
+      name: 'Mute',
+      imdb: '5.5',
+      plot: "In a Berlin of the future, a mute bartender's search for his missing lady-love takes him deeper and deeper into the city's criminal underbelly.",
+    },
+    {
+      name: 'Prisoners',
+      imdb: '8.1',
+      plot: "When the police take time to find Keller Dover's daughter and her friend, he decides to go on a search himself. His desperation leads him closer to finding the truth and also jeopardises his own life.",
+    },
+  ],
+  Thriller: [
+    {
+      name: 'Mirage',
+      imdb: '7.4',
+      plot: "A space-time continuum glitch allows Vera to save a boy's life 25 years in the past, but somehow results in the loss of her daughter and Vera must fight to get her back.",
+    },
+    {
+      name: 'Wrath of man',
+      imdb: '7.1',
+      plot: 'H is a mysterious man who starts working for a cash moving truck company. He becomes known for using amazing precision and dexterity to neutralise robbers. However, H is actually out for revenge.',
+    },
+    {
+      name: 'A quiet place 2',
+      imdb: '7.3',
+      plot: 'In a post-apocalyptic world, Regan and her family learn that the alien predators can be defeated using high-frequency audio. Armed with this knowledge, they then set out to look for other survivors.',
+    },
+    {
+      name: 'Old',
+      imdb: '5.8',
+      plot: 'A thriller about a family on a tropical holiday who discover that the secluded beach where they are relaxing for a few hours is somehow causing them to age rapidly reducing their entire lives into a single day.',
+    },
+  ],
+  Comedy: [
+    {
+      name: 'Jungle Cruise',
+      imdb: '6.6',
+      plot: 'Dr. Lily Houghton enlists the aid of wisecracking skipper Frank Wolff to take her down the Amazon in his ramshackle boat. Together, they search for an ancient tree that holds the power to heal -- a discovery that will change the future of medicine.',
+    },
+    {
+      name: 'Free guy',
+      imdb: '7.2',
+      plot: "When a bank teller discovers he's actually a background player in an open-world video game, he decides to become the hero of his own story -- one that he can rewrite himself. In a world where there's no limits, he's determined to save the day his way before it's too late, and maybe find a little romance with the coder who conceived him.",
+    },
+    {
+      name: 'Dolittle',
+      imdb: '5.6',
+      plot: 'A physician, who can speak with animals, is summoned by the queen to find a cure for her life-threatening disease. Soon, he embarks on a journey with his furry friends in search of a healing tree.',
+    },
+    {
+      name: 'Tag',
+      imdb: '6.5',
+      plot: 'A group of five friends, who have been playing the game of tag for 30 years, decide to play one last game before the wedding of their undefeated player.',
+    },
+  ],
+};
+
+export default movies;
